733c62b26563dbfd0e2258853ae6c1826a817af3
techinvestorsza <techinvestorsza@gmail.com>
2023-11-12 13:53:39 +0200

    Completed the orders page, inital phase of the ecommerce page

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Tooltip, ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries } from '@syncfusion/ej2-react-charts'
 
-const Stacked = () => {
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy'
+
+const Stacked = ({ width, height }) => {
   return (
-    <div>Stacked</div>
+    <ChartComponent
+      width={width}
+      height={height}
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 }}}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { ContextProvider } from './contexts/ContextProvider'
+
 // Hook react app to the root div
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>, document.getElementById('root'));
