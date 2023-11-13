ea26e06aa276624d2001948287f4505a0810d29c
techinvestorsza <techinvestorsza@gmail.com>
2023-11-13 15:01:57 +0200

    Updated the Customers page with editing

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Page, GridComponent, ColumnsDirective, ColumnDirective, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids'
+
+import { customersData, customersGrid } from '../data/dummy'
+import { Header } from '../components';
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title='Customers' />
+      <GridComponent
+        id='gridcomp'
+        dataSource={customersData}
+        // enable pagination
+        allowPaging
+        allowSorting
+        toolbar={[ 'Delete']}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width='auto'
+      >
+        <ColumnsDirective>
+           {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+           ))}
+        </ColumnsDirective>
+
+        {/* enable pagination */}
+        <Inject services={[ Page, Toolbar, Selection, Edit, Sort, Filter]} />
+
+      </GridComponent>
+    </div>
   )
 }
 
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids'
 
-import { employeesData, contextMenuItems, employeesGrid } from '../data/dummy'
+import { employeesData, employeesGrid } from '../data/dummy'
 import { Header } from '../components'
 
 const Employees = () => {
